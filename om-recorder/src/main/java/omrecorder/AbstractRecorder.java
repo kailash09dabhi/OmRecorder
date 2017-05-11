3dba09a6fd86d99b00040c895cf26e7776e17474
Kailash Dabhi <kailash09dabhi@gmail.com>
2017-05-11 13:21:25 +0530

    Throw exceptions instead of suppressing

--- om-recorder/src/main/java/omrecorder/AbstractRecorder.java
+++ om-recorder/src/main/java/omrecorder/AbstractRecorder.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -23,7 +23,7 @@ import java.io.OutputStream;
 
 /**
  * @author Kailash Dabhi
- * @date  22-08-2016.
+ * @date 22-08-2016.
  * Copyright (c) 2017 Kingbull Technology. All rights reserved.
  */
 abstract class AbstractRecorder implements Recorder {
@@ -37,28 +37,30 @@ abstract class AbstractRecorder implements Recorder {
     this.outputStream = outputStream(file);
   }
 
-  @Override public void startRecording() {
-    new Thread(new Runnable() {
-      @Override public void run() {
-        try {
-          pullTransport.start(outputStream);
-        } catch (IOException e) {
-          new RuntimeException(e);
-        }
-      }
-    }).start();
-  }
-
   private OutputStream outputStream(File file) {
     if (file == null) throw new RuntimeException("file is null !");
     OutputStream outputStream;
     try {
       outputStream = new FileOutputStream(file);
     } catch (FileNotFoundException e) {
-      throw new RuntimeException("could not build OutputStream from" +
-          " this file" + file.getName(), e);
+      throw new RuntimeException(
+          "could not build OutputStream from" + " this file" + file.getName(), e);
     }
     return outputStream;
+  }
+
+  @Override public void startRecording() {
+    new Thread(new Runnable() {
+      @Override public void run() {
+        try {
+          pullTransport.start(outputStream);
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        } catch (IllegalStateException e) {
+          throw new RuntimeException("AudioRecord state has uninitialized state", e);
+        }
+      }
+    }).start();
   }
 
   @Override public void stopRecording() {
